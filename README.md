6a3704ab8c2a2c0a06675bc1ad1215cb8cc88860
chegde <chegde@foxmail.com>
2018-11-10 11:20:16 +0800

    chore(zip): add mode choice.

---
+++ README.md
@@ -0,0 +1,9 @@
+# 快速打包dist文件夹
+
+例子： 
+1. node ./fast-zip-folder/zip.js --mode=development
+2. node ./fast-zip-folder/zip.js --mode=production
+
+```
+output folder-name + mode-name + timestamp .zip
+```
--- zip.js
+++ zip.js
@@ -1,13 +1,17 @@
 const process = require('process');
 const path = require("path");
 const AdmZip = require('adm-zip');
+const modeOriginString = process.argv.slice(2)[0];
+let mode = 'development';
+if (modeOriginString && modeOriginString.indexOf('=') >= 0) {
+    mode = modeOriginString.split('=')[1];
+}
 
 const targetPath = path.resolve('./', 'dist');
 
-
-var zip = new AdmZip();
+const zip = new AdmZip();
 zip.addLocalFolder(targetPath);
 const timestamp = new Date().getTime();
 const dirname = process.cwd().split('\\').pop();
 
-zip.writeZip(`${dirname}-${timestamp}.zip`);
+zip.writeZip(`${dirname}-${mode}-${timestamp}.zip`);
